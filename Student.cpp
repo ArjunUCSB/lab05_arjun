93bb3d379bfd107bbf4f8ea66c3327c38bd420c0
ArjunUCSB <arjun_shah@ucsb.edu>
2025-11-10 21:03:18 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -2,57 +2,41 @@
 #include <sstream>
 #include <string>
 
-Student::Student(int perm, 
-		 std::string lastName, 
-		 std::string firstAndMiddleNames) {
-  // stub
+Student::Student(int perm, std::string lastName, std::string firstAndMiddleNames) {
+  this->perm = perm;
+  this->lastName = lastName;
+  this->firstAndMiddleNames = firstAndMiddleNames;
 }
 
-// construct a Student object from a single line of
-// comma separated text, e.g. "1234567,Smith,Mary Kay"
-
-Student::Student (std::string csvString) {
-
+Student::Student(std::string csvString) {
   std::istringstream iss(csvString);
-
-  // This version of getline takes an input string (istream), a string, 
-  // and a delimiter character (in this case, comma).
-  // We read from the stream until the delimiter is encountered 
-  
   std::string permAsString;
   getline(iss, permAsString, ',');
-  this->perm = std::stoi(permAsString); // stoi converts std::string to int
-
+  this->perm = std::stoi(permAsString);
   getline(iss, this->lastName, ',');
-
   getline(iss, this->firstAndMiddleNames, '\n');
 }
 
-
-int Student::getPerm() const { 
-  return -999; // stub
+int Student::getPerm() const {
+  return perm;
 }
 
-std::string Student::getLastName() const { 
-  return std::string("Stuberson");
+std::string Student::getLastName() const {
+  return lastName;
 }
 
 std::string Student::getFirstAndMiddleNames() const {
-   return std::string("Stub Toe");
+  return firstAndMiddleNames;
 }
 
-std::string Student::getFullName() const { 
-  return std::string("Stub Toe Stuberson");
+std::string Student::getFullName() const {
+  return firstAndMiddleNames + " " + lastName;
 }
 
-std::string Student::toString() const { 
-  // e.g. [12345,Smith,Malory Logan]
+std::string Student::toString() const {
   std::ostringstream oss;
-  
-  oss << "[" 
-      << this->getPerm() << ","
-      << this->getLastName() << ","
-      << this->getFirstAndMiddleNames() << "]";
+  oss << "[" << perm << "," << lastName << "," << firstAndMiddleNames << "]";
   return oss.str();
-} 
+}
+
 
